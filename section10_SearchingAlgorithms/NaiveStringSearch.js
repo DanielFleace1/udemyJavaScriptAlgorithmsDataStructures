60a8ed6dfd86071fbb0595a048afba4a334d81f8
Daniel Fleace <danielfleace15@gmail.com>
2022-02-24 09:35:30 -0500

    Naive String Search (simple)

---
+++ section10_SearchingAlgorithms/NaiveStringSearch.js
@@ -0,0 +1,22 @@
+
+
+
+function naiveSearch(short,long){
+    let counter = 0;
+    for(let i = 0;i<long.length;i++){
+        for(let j = 0;j<short.length;j++){
+            if(short[j] !== long[i+j]){
+                break
+            }
+            else if(j === short.length-1){
+                counter++;
+            }
+        }
+    }
+    console.log(counter)
+    return counter
+}
+
+naiveSearch('and','dandandaniel')
+//dan
+//dandandaniel
